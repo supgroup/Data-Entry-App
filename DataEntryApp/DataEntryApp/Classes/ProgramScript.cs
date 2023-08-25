b82b68d33148467b3384a2035a58fe8eb806e32e
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2023-08-25 19:37:58 +0300

    sql

---
+++ DataEntryApp/DataEntryApp/Classes/ProgramScript.cs
@@ -0,0 +1,178 @@
+﻿using DataEntryApp.ApiClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.IO;
+ 
+namespace DataEntryApp.Classes
+{
+    public class ProgramScript
+    {
+      public  static void Main(string DataSource,string databas,string user,string pass)
+        {
+            try {
+                string connectionString = "Data Source="+DataSource+"; Initial Catalog="+ databas + ";User ID="+ user + ";Password="+ pass;
+
+            // اتصال بقاعدة البيانات
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // استعراض الجداول في قاعدة البيانات
+                string[] tables = GetTables(connection);
+                string script = "";
+                // توليد نص SQL لكل جدول
+                foreach (string table in tables)
+                {
+                      script += GenerateTableScript(connection, table);
+
+                    // طباعة نص SQL للجدول
+                 //   Console.WriteLine(script);
+                }
+                    // توليد نص SQL للعلاقات بين الجداول
+                    string relationshipsScript = GenerateRelationshipsScript(connection);
+                    script += relationshipsScript;
+                    Console.WriteLine(relationshipsScript);
+                    File.WriteAllText("D:/scrpt111.sql", script);
+            }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        static string[] GetTables(SqlConnection connection)
+        {
+            // استعلام لاستعراض الجداول في قاعدة البيانات
+            string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                // جمع أسماء الجداول
+                var tables = new List<string>();
+
+                while (reader.Read())
+                {
+                    string tableName = reader.GetString(0);
+                    tables.Add(tableName);
+                }
+
+                return tables.ToArray();
+            }
+        }
+
+        static string GenerateTableScript(SqlConnection connection, string tableName)
+        {
+            // استعلام لاستعراض هيكل الجدول والسجلات
+            string query = $"SELECT * FROM {tableName};";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                // بناء نص SQL للجدول
+                StringBuilder scriptBuilder = new StringBuilder();
+
+                // إضافة جدول
+                scriptBuilder.AppendLine($"-- توليد جدول: {tableName}");
+                scriptBuilder.AppendLine("CREATE TABLE " + tableName);
+                scriptBuilder.AppendLine("(");
+
+                // إضافة أعمدة الجدول
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string columnName = reader.GetName(i);
+                    string columnType = reader.GetDataTypeName(i);
+
+                    scriptBuilder.AppendLine($"    {columnName} {columnType},");
+
+                    // إذا كان العمود هو العمود الأخير، قم بإزالة الفاصلة
+                    if (i == reader.FieldCount - 1)
+                    {
+                        scriptBuilder.Length -= 3;
+                    }
+                }
+
+                scriptBuilder.AppendLine();
+                scriptBuilder.AppendLine(");");
+
+                scriptBuilder.AppendLine();
+
+                // إضافة سجلات الجدول
+                if (reader.HasRows)
+                {
+                    scriptBuilder.AppendLine($"-- إدراج سجلات الجدول: {tableName}");
+
+                    while (reader.Read())
+                    {
+                        scriptBuilder.AppendLine("INSERT INTO " + tableName + " VALUES (");
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            string value = reader.IsDBNull(i) ? "NULL" : "'" + reader.GetValue(i).ToString().Replace("'", "''") + "'";
+                            scriptBuilder.Append($"    {value},");
+                        }
+
+                        scriptBuilder.Length -= 1; // إزالة الفاصلة الأخيرة
+                        scriptBuilder.AppendLine(");");
+                    }
+                }
+
+                return scriptBuilder.ToString();
+            }
+        }
+        static string GenerateRelationshipsScript(SqlConnection connection)
+        {
+            StringBuilder scriptBuilder = new StringBuilder();
+            scriptBuilder.AppendLine("-- توليد العلاقات");
+
+            string query = @"
+                SELECT
+                    FK.TABLE_NAME AS TableName,
+                    CU.COLUMN_NAME AS ColumnName,
+                    PK.TABLE_NAME AS ReferencedTableName,
+                    PT.COLUMN_NAME AS ReferencedColumnName
+                FROM
+                    INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS C
+                    INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS FK ON C.CONSTRAINT_NAME = FK.CONSTRAINT_NAME
+                    INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS PK ON C.UNIQUE_CONSTRAINT_NAME = PK.CONSTRAINT_NAME
+                    INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE CU ON C.CONSTRAINT_NAME = CU.CONSTRAINT_NAME
+                    INNER JOIN (
+                        SELECT
+                            i1.TABLE_NAME,
+                            i2.COLUMN_NAME
+                        FROM
+                            INFORMATION_SCHEMA.TABLE_CONSTRAINTS i1
+                            INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE i2 ON i1.CONSTRAINT_NAME = i2.CONSTRAINT_NAME
+                        WHERE
+                            i1.CONSTRAINT_TYPE = 'PRIMARY KEY'
+                    ) PT ON PT.TABLE_NAME = PK.TABLE_NAME
+                WHERE
+                    FK.TABLE_NAME <> PK.TABLE_NAME;
+            ";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string tableName = reader.GetString(0);
+                    string columnName = reader.GetString(1);
+                    string referencedTableName = reader.GetString(2);
+                    string referencedColumnName = reader.GetString(3);
+
+                    scriptBuilder.AppendLine($"ALTER TABLE {tableName}");
+                    scriptBuilder.AppendLine($"ADD CONSTRAINT FK_{tableName}_{columnName}");
+                    scriptBuilder.AppendLine($"FOREIGN KEY ({columnName})");
+                    scriptBuilder.AppendLine($"REFERENCES {referencedTableName}({referencedColumnName});");
+
+                    scriptBuilder.AppendLine();
+                }
+            }
+
+            return scriptBuilder.ToString();
+        }
+    }
+}
---
+++ DataEntryApp/DataEntryApp/Classes/SmoScript.cs
@@ -0,0 +1,154 @@
+﻿using DataEntryApp.ApiClasses;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+//using System.Data.SqlClient;
+using System.Collections.Specialized;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+namespace DataEntryApp.Classes
+{
+    public class SmoScript
+    {
+        public static void Main(string DataSource, string databas, string user, string pass)
+        {
+         //   string connectionString = "Data Source=<server>;Initial Catalog=<database>;User ID=<username>;Password=<password>";
+            string connectionString = "Data Source=" + DataSource + "; Initial Catalog=" + databas + ";User ID=" + user + ";Password=" + pass;
+
+            // تكوين معلومات الاتصال
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+
+            // إنشاء كائن ServerConnection باستخدام معلومات الاتصال
+            ServerConnection serverConnection = new ServerConnection(builder.DataSource);
+            serverConnection.DatabaseName = builder.InitialCatalog;
+            serverConnection.LoginSecure = false;
+            serverConnection.Login = builder.UserID;
+            serverConnection.Password = builder.Password;
+
+            // اتصال بخادم SQL
+            Server server = new Server(serverConnection);
+
+            // استعراض الجداول في قاعدة البيانات
+            List<string> tables = GetTables(server);
+            string script = "";
+            // توليد نص SQL لكل جدول
+            foreach (string table in tables)
+            {
+               script += GenerateTableScript(server, table);
+                //Console.WriteLine(script);
+            }
+
+            // توليد نص SQL للعلاقات بين الجداول
+
+            string relationshipsScript = GenerateRelationshipsScript(server);
+            script += relationshipsScript;
+            File.WriteAllText("D:/scrpt111.sql", script);
+         //   Console.WriteLine(relationshipsScript);
+        }
+
+        static List<string> GetTables(Server server)
+        {
+            List<string> tables = new List<string>();
+
+            foreach (Table table in server.Databases[server.ConnectionContext.DatabaseName].Tables)
+            {
+                if (table.Name != "sysdiagrams")
+                {
+                    tables.Add(table.Name);
+                }
+                 
+            }
+
+            return tables;
+        }
+
+        static string GenerateTableScript(Server server, string tableName)
+        {
+            StringBuilder scriptBuilder = new StringBuilder();
+
+            // استعلام عن الجدول
+            Table table = server.Databases[server.ConnectionContext.DatabaseName].Tables[tableName];
+
+            // توليد نص SQL للجدول
+            ScriptingOptions options = new ScriptingOptions();
+            options.IncludeHeaders = true;
+            options.SchemaQualify = true;
+            options.Default = true;
+            options.ExtendedProperties = true;
+        //  options.IncludeDatabaseContext = true;
+            options.DriPrimaryKey = true;
+            //options.DriForeignKeys = true;
+            //options.SchemaQualifyForeignKeysReferences = true;
+           
+            // options.PrimaryObject = true;
+            //  options.SetTargetServerVersion=ServerVersion()
+            options.ScriptDrops = false;
+            //options.ScriptForCreateDrop = true;
+          
+            options.IncludeIfNotExists = true;
+            options.ScriptData = true;
+            options.ScriptSchema = true;
+            //options.ExcludeObjectTypes = new[] { typeof(Diagram) };
+            //StringCollection tableScripts = table.Script(options);
+            //
+            Scripter scripter = new Scripter(server);
+            scripter.Options = options;
+       
+          
+            IEnumerable<string> tableScripts = scripter.EnumScript(new Urn[] { table.Urn });
+            //  StringCollection dataScripts = scripter.EnumScript(new Urn[] { table.Urn });
+            IEnumerable<string> dataScripts = scripter.EnumScript(new Urn[] { table.Urn });
+            // توليد نص SQL للجدول
+            foreach (string tableScript in tableScripts)
+            {
+                scriptBuilder.AppendLine(tableScript);
+                scriptBuilder.AppendLine();
+            }
+
+            // توليد نص SQL للبيانات
+            //foreach (string dataScript in dataScripts)
+            //{
+            //    scriptBuilder.AppendLine(dataScript);
+            //    scriptBuilder.AppendLine();
+            //}
+            ///
+            //foreach (string tableScript in tableScripts)
+            //{
+            //    scriptBuilder.AppendLine(tableScript);
+            //    scriptBuilder.AppendLine();
+            //}
+
+            return scriptBuilder.ToString();
+        }
+
+        static string GenerateRelationshipsScript(Server server)
+        {
+            StringBuilder scriptBuilder = new StringBuilder();
+            Database database = server.Databases[server.ConnectionContext.DatabaseName];
+
+            // استعلام عن العلاقات
+            foreach (Table table in database.Tables)
+            {
+                foreach (ForeignKey foreignKey in table.ForeignKeys)
+                {
+                    string tableName = table.Name;
+                    string columnName = foreignKey.Columns[0].Name;
+                    string referencedTableName = foreignKey.ReferencedTable;
+                  //  string referencedColumnName = foreignKey.ReferencedColumns[0].Name;
+                    string referencedColumnName = foreignKey.Columns[0].ReferencedColumn;
+                    scriptBuilder.AppendLine($"ALTER TABLE {tableName}");
+                    scriptBuilder.AppendLine($"ADD CONSTRAINT FK_{tableName}_{columnName}");
+                    scriptBuilder.AppendLine($"FOREIGN KEY ({columnName})");
+                    scriptBuilder.AppendLine($"REFERENCES {referencedTableName}({referencedColumnName});");
+
+                    scriptBuilder.AppendLine();
+                }
+            }
+
+            return scriptBuilder.ToString();
+        }
+    }
+}
